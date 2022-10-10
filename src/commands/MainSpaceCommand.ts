18df94bcbd9e68bd9651878c6ef9945e476b43ae
Jeffrey <jeffreyhu16@gmail.com>
2022-10-10 11:29:19 +0800

    add camera state

--- src/commands/MainSpaceCommand.ts
+++ src/commands/MainSpaceCommand.ts
@@ -1,5 +1,6 @@
 import { Command } from "@colyseus/command";
 import { MainSpaceRoom } from "../rooms/MainSpaceRoom";
+import { CameraState } from "../schema/CameraState";
 import { PlayerState } from "../schema/PlayerState";
 import { PositionState } from "../schema/PositionState";
 
@@ -11,22 +12,27 @@ export class OnJoinCommand extends Command<MainSpaceRoom, {
     const playerCount = this.state.players.size;
     let position: PositionState;
     let rotation: number;
+    let alpha: number;
 
     switch (playerCount) {
       case 0:
         position = new PositionState(0, -5, 10);
         rotation = 0;
+        alpha = Math.PI / 2;
         break;
       case 1:
         position = new PositionState(10, -5, 0);
         rotation = 90;
+        alpha = Math.PI * 2;
         break;
       case 2:
         position = new PositionState(-10, -5, 0);
         rotation = -90;
+        alpha = Math.PI;
         break;
     }
     this.state.players.set(sessionId, new PlayerState(name, position, rotation));
+    this.state.camera = new CameraState(alpha, position);
   }
 }
 
--- src/schema/MainSpaceState.ts
+++ src/schema/MainSpaceState.ts
@@ -1,8 +1,11 @@
 import { Schema, type, MapSchema } from "@colyseus/schema";
+import { CameraState } from "./CameraState";
 import { PlayerState } from "./PlayerState";
 
 export class MainSpaceState extends Schema {
 
   @type({ map: PlayerState }) players = new MapSchema<PlayerState>();
+
+  @type(CameraState) camera: CameraState;
 
 }
